721fdf5bf9119e3a6353bf3945599f0245aeaed7
Ilya Urikh <ilya.bti@mail.ru>
2012-12-04 09:32:45 +0400

    Main project was added.

---
+++ source/MigrationTool/Bootstrapper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Practices.Prism.UnityExtensions;
+
+namespace MigrationTool
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override System.Windows.DependencyObject CreateShell()
+        {
+            return new Shell();
+        }
+
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+
+            Application.Current.MainWindow = (Window) Shell;
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureModuleCatalog()
+        {
+            base.ConfigureModuleCatalog();
+
+        }
+    }
+}
---
+++ source/MigrationTool/Shell.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace MigrationTool
+{
+    /// <summary>
+    /// Interaction logic for Shell.xaml
+    /// </summary>
+    public partial class Shell
+    {
+        public Shell()
+        {
+            InitializeComponent();
+        }
+    }
+}
