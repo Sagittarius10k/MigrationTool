837ee467c442b54120e1122f07042c22f01f4d24
Ilya Urikh <ilya.bti@mail.ru>
2013-02-14 18:03:29 +0400

    ProjectManager were added.

---
+++ source/MigrationTool.Modules.Projects/ProjectManager.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.Composition;
+using MigrationTool.Infrastructure;
+using MigrationTool.Infrastructure.Configuration;
+
+namespace MigrationTool.Modules.Projects
+{
+    public class ProjectManager
+    {
+#pragma warning disable 0649
+        [Import] private ConfigurationManager _configuration;
+#pragma warning restore 0649
+
+
+    }
+}
